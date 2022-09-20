b717c7c17e5668121626818b20b3bf2524c0776e
jeff kang <jeffyjkang@gmail.com>
2022-09-20 14:50:37 -0500

    init project 1

---
+++ project_1/contracts/Inbox.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.16;
+
+contract Inbox {
+  string private message;
+
+  constructor(string memory initialMessage) {
+    message = initialMessage;
+  }
+
+  function setMessage(string calldata newMessage) public {
+    message = newMessage;
+  }
+
+  function getMessage() public view returns (string memory) {
+    return message;
+  }
+
+  // function doMath(int a, int b) {
+  //   a + b;
+  //   b - a;
+  //   a * b;
+  //   a == 0;
+  // }
+}
