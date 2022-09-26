a468ba8cdb9d66ed11b69bafe186a5b2cf0dcbcc
jeff kang <jeffyjkang@gmail.com>
2022-09-26 18:22:47 -0500

    project 4 tests, deploy config

--- project_4/migrations/1_deploy_contracts.js
+++ project_4/migrations/1_deploy_contracts.js
@@ -1,6 +1,9 @@
 // const Campaign = artifacts.require('Campaign');
 const CampaignFactory = artifacts.require('CampaignFactory');
+const Campaign = artifacts.require('Campaign');
 
 module.exports = (deployer) => {
   deployer.deploy(CampaignFactory);
+  // second argument is supposed to be creator/manager
+  // deployer.deploy(Campaign, 1, '0xEfcA720FbDfFb296023BE24f9ad0268da102CE1d')
 }
